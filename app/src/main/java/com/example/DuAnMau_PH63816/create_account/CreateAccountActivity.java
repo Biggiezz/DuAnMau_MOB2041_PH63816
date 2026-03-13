d19aaf330cd1457423461ca4b3b4b0d574819de5
manh phuc <manhphuc300501@gmail.com>
2026-03-13 15:47:30 +0700

    create CreateAccountActivity.java

---
+++ app/src/main/java/com/example/DuAnMau_PH63816/create_account/CreateAccountActivity.java
@@ -0,0 +1,40 @@
+package com.example.DuAnMau_PH63816.create_account;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.DuAnMau_PH63816.R;
+import com.example.DuAnMau_PH63816.login.LoginScreen;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    private ImageView icBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_account);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initUi();
+        icBack.setOnClickListener(v -> {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginScreen.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void initUi() {
+        icBack = findViewById(R.id.icBack);
+    }
+}
