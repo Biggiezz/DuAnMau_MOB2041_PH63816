8ab7b59241f34e1ab9359ce89b34137d6415c883
manh phuc <manhphuc300501@gmail.com>
2026-03-16 13:29:31 +0700

    update home page screen layout and logic

--- app/src/main/java/com/example/DuAnMau_PH63816/homepage/HomePageScreen.java
+++ app/src/main/java/com/example/DuAnMau_PH63816/homepage/HomePageScreen.java
@@ -1,26 +1,74 @@
 package com.example.DuAnMau_PH63816.homepage;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.DuAnMau_PH63816.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePageScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initUi();
+
     }
+
+    private void initUi() {
+        DrawerLayout drawerLayout = findViewById(R.id.main);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        Toolbar toolbarHomePage = findViewById(R.id.toolbarHomePage);
+
+        /// toolbar
+        setSupportActionBar(toolbarHomePage);
+        toolbarHomePage.setNavigationIcon(R.drawable.btn_menu);
+
+        toolbarHomePage.setNavigationOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(item -> {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            });
+        }
+    }
+
+    /// Cách sử dụng Toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        /// lấy id của các item
+        int id = item.getItemId();
+        if (id == R.id.notification) {
+            Toast.makeText(this, "Thông báo", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
