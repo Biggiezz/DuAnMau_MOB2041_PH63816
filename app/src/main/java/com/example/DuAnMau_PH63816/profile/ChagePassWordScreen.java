0787264372b095b252bb11d3c077661ddfc2992f
manh phuc <manhphuc300501@gmail.com>
2026-03-17 15:52:17 +0700

    create change password screen

--- app/src/main/java/com/example/DuAnMau_PH63816/profile/ChagePassWordScreen.java
+++ app/src/main/java/com/example/DuAnMau_PH63816/profile/ChagePassWordScreen.java
@@ -1,9 +1,6 @@
 package com.example.DuAnMau_PH63816.profile;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,29 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.DuAnMau_PH63816.R;
 
-public class ProfileScreen extends AppCompatActivity {
+public class ChagePassWordScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile_screen);
+        setContentView(R.layout.activity_chage_pass_word_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        bindDataFromIntent();
     }
-
-    private void bindDataFromIntent() {
-        Intent intent = getIntent();
-        if (intent == null) return;
-
-        ImageView icBack = findViewById(R.id.icBack);
-        LinearLayout linearChagePass = findViewById(R.id.linearChagePass);
-        linearChagePass.setOnClickListener(v -> startActivity(new Intent(ProfileScreen.this, ChagePassWordScreen.class)));
-        icBack.setOnClickListener(v -> finish());
-    }
-
 }
